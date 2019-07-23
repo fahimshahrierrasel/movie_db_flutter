cd5d5942f0371cb57e7498634cf8a0825124dddc
Md. Fahim Shahrier Rasel <fahim.handymama@gmail.com>
2019-07-23 19:19:19 +0600

    discover movie ui updated

--- lib/main.dart
+++ lib/main.dart
@@ -39,9 +39,50 @@ class _DiscoverMoviesState extends State<DiscoverMovies> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            ExpansionTile(
-              title: Text("Discover Movies"),
-              children: <Widget>[Text("Hello"), Text("World")],
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 3,
+                            child: TextField(
+                              decoration:
+                                  InputDecoration(hintText: "Search Here"),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: MaterialButton(
+                                child: Text("Search"), onPressed: () {}),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: ExpansionTile(
+                        title: Text("Advance Search"),
+                        children: <Widget>[
+                          new DropdownButton<String>(
+                            items: <String>["2017", "2016"].map((String value) {
+                              return new DropdownMenuItem<String>(
+                                value: value,
+                                child: new Text(value),
+                              );
+                            }).toList(),
+                            onChanged: (_) {},
+                          ),
+                          Slider(value: 100, min: 0, max: 100, onChanged: null)
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
             Expanded(child: MovieList())
           ],
